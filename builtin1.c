6bac5330b0d5b45ed79ebb171b8b04d07cb7b4aa
amandamadlingozi <rjmadlingozi@gmail.com>
2023-11-07 03:02:33 +0200

    builtin1

---
+++ builtin1.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _myhistory - presents the command history as a sequentially numbered list,
+ * showing one command per line, beginning with line number 0.
+ *
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
+ *
+ * Return: Always 0
+ */
